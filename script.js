2fc83327460df4d24d6be2f4481b586793852df9
slimnation23 <svetaberynda@gmail.com>
2023-07-09 07:23:31 +0200

    menuDone

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuBtn = document.querySelector(".menu-toggle");
 const navList = document.querySelector(".nav-list");
+const navLink = document.querySelectorAll(".nav-link");
 
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   navList.classList.toggle("active");
+
+  navLink.forEach((item) => {
+    item.addEventListener('click', () => {
+      navList.classList.remove("active");
+    })
+  })
 }
